705eaad177c6645fcfa1965e5ba7c8341b2f7257
vasant-deb <iamvasantsharma@gmail.com>
2023-09-07 21:41:35 -0400

    icon-fix

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,7 @@ self.addEventListener('install', function (event) {
           '/src/css/app.css',
           '/src/images/pwa.jpg',
           'https://fonts.googleapis.com/css?family=Raleway:400,700',
-		  'https://kit.fontawesome.com/1ee8f271b9.js'
+		  'src/js/kit.js'
         ]);
       })
   );
