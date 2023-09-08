3de7fbe39a3eb993b2c5e0eb2116273b654d6a42
vasant-deb <iamvasantsharma@gmail.com>
2023-09-07 22:02:06 -0400

    bg-fix

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@ self.addEventListener('install', function (event) {
           '/src/images/pwa.jpg',
           'https://fonts.googleapis.com/css?family=Raleway:400,700',
 		  'src/js/kit.js',
-		  'https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80'
+		  '/src/images/bg.avif'
         ]);
       })
   );
