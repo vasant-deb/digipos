726bd92d129ff019f5110fa84fda3e944d7efba0
vasant-deb <iamvasantsharma@gmail.com>
2023-09-19 02:33:54 -0400

    update1.1

--- public/sw.js
+++ public/sw.js
@@ -3,10 +3,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('static')
       .then(function (cache) {
-        // cache.add('/');
-        // cache.add('/index.html');
-        // cache.add('/src/js/app.js');
-        cache.addAll([
+        return cache.addAll([
           '/',
           '/index.html',
           '/sales.html',
@@ -17,7 +14,7 @@ self.addEventListener('install', function (event) {
           '/src/js/toast.js',
           '/src/js/sales.js',
           '/src/js/time.js',
-		      '/src/js/jquery.js',
+          '/src/js/jquery.js',
           '/src/js/app.js',
           '/src/css/app.css',
           '/src/images/favicon/apple-touch-icon.png',
@@ -25,26 +22,31 @@ self.addEventListener('install', function (event) {
           '/src/images/favicon/favicon-16x16.png',
           '/src/images/favicon/site.webmanifest',
           'https://fonts.googleapis.com/css?family=Oswald',
-		      '/src/js/kit.js',
-		      '/src/images/bg.avif'
+          '/src/js/kit.js',
+          '/src/images/bg.avif'
         ]);
       })
   );
 });
 
-self.addEventListener('activate', function () {
+self.addEventListener('activate', function (event) {
   console.log('SW Activated');
+  // Clean up old caches here if necessary
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(res) {
-        if (res) {
-          return res;
-        } else {
-          return fetch(event.request);
-        }
+      .then(function (res) {
+        return res || fetch(event.request)
+          .then(function (response) {
+            // Cache the fetched response for future use
+            return caches.open('static')
+              .then(function (cache) {
+                cache.put(event.request, response.clone());
+                return response;
+              });
+          });
       })
   );
 });
